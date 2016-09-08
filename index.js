3721a571e04c3d5b8225c8ca47d1ad2211cab9bc
Chelsea Casareale <chlcrc@gmail.com>
2016-09-08 02:22:07 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function iterativeLog(array) {
+  array.forEach((element, index) => {
+    console.log(`${index}: ${element}`)
+  })
+}
+
+function iterate(callback) {
+  var myArray = ['a', 'b', 'c'];
+
+  myArray.forEach(callback);
+
+  return myArray;
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback);
+}
